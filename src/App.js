f29aa64aeb60d1c4aa3d30f731efeea935face89
Toni Dedić <dedictoni1@gmail.com>
2022-01-15 18:26:44 +0100

    added calculator

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import ToDoList from './components/to-do-list/ToDoList';
 import Hangman from './components/hangman/Hangman';
 import TicTacToeGame from './components/tic-tac-toe/TicTacToeGame';
+import Fields from './components/calculator/Fields';
 
 function App() {
   return (
     <div>
+      <Fields />
       <ToDoList />
       <Hangman />
       <TicTacToeGame />
